2a39e8b7fba54d7f13d2d23267b4382c48426911
Tom Chen <ch271828n@outlook.com>
2026-08-11 08:03:22 +0800

    Apply CI formatter output

--- tests/fast/utils/workers/test_argv_utils.py
+++ tests/fast/utils/workers/test_argv_utils.py
@@ -108,9 +108,7 @@ class TestConfigToArgv:
             lora_adapter_path=None,
             use_session_server="v2",
             session_sample_picker_path="miles.rollout.session.v2.picker_hub.drop_retries",
-            session_sample_postprocessor_path=(
-                "miles.rollout.session.v2.postprocessor_hub.default_postprocess"
-            ),
+            session_sample_postprocessor_path=("miles.rollout.session.v2.postprocessor_hub.default_postprocess"),
         )
         assert parse_config_argv(SessionServerConfig, config_to_argv(session_config)) == session_config
 
